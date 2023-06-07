5f97b0077471272b234244980bf73cd10eccd7a2
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-06-06 23:58:50 -0500

    ingresando routes con PHP y modularizacion del codigo

---
+++ views/header/index.php
@@ -0,0 +1,11 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <img src="<?php echo constant('URL') ?>public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
+    </a>
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Inicio</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Servicios</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Acerca de</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contacto</a></li>
+    </ul>
+</header>
---
+++ views/main/index.php
@@ -0,0 +1,8 @@
+
+<?php
+require 'views/index/index.php';
+require 'views/header/index.php';
+?>
+<div id="main">
+    <h1 class="center">Bienvenido al sitio de index</h1>
+</div>
