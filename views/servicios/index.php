a26ce44c36b008bc029fba6f7cdd92319b3dc345
Alejandra Roa <malehis@gmail.com>
2023-06-10 21:38:05 -0500

    cambio de imagenes para cards

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -24,12 +24,12 @@ require 'views/header/index.php';
                     <p class="card-text mb-auto">En construcción es una herramienta de uso en distintas fases de un proyecto.</p>
                     <a class="continua"class="continua" href="#"><br>Continua leyendo...</a>
                 </div>
-                <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/cocina.jpg" data-holder-rendered="true">
+                <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/arquitectura.png" data-holder-rendered="true">
             </div>
         </div>
         <div class="col-lg-6">
             <div class="card flex-md-row mb-4 box-shadow h-md-250">
-                <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/casa_test.png" data-holder-rendered="true">
+                <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/inmobiliaria.png" data-holder-rendered="true">
                 <div class="card-body d-flex flex-column align-items-start">
                     <strong class="d-inline-block mb-2 text-primary">INMOBILIARIO Y COMERCIAL</strong>
                     <p class="card-text mb-auto">Las visitas virtuales ofrecen a los agentes inmobiliarios la oportunidad de resaltar las características únicas.</p>
@@ -46,12 +46,12 @@ require 'views/header/index.php';
                         <p class="card-text mb-auto">Las visitas virtuales 360 en hostelería ofrecen una serie de ventajas para los negocios del sector.</p>
                         <a class="continua"href="#">Continua leyendo...</a>
                     </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/cocina.jpg" data-holder-rendered="true">
+                    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/hosteleria.png" data-holder-rendered="true">
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="card flex-md-row mb-4 box-shadow h-md-250">
-                    <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/patrimonio_test.png" data-holder-rendered="true">
+                    <img class="card-img-left flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 300px; height: auto;" src="<?php echo constant('URL') ?>public/images/seguros.png" data-holder-rendered="true">
                     <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">SEGUROS Y PATRIMONIO</strong>
                         <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
