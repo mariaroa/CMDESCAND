e20f2c7c2c83bb80267b3d5ffb56f06674d86bc9
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-01 00:46:01 -0500

    agregando al header

--- header.php
+++ header.php
@@ -1,11 +1,27 @@
-<header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img src="<?php echo constant('URL') ?>public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
-    </a>
-    <ul class="nav nav-pills">
-        <li class="nav-item"><a href="http://localhost/" class="nav-link" id="menu-inicio">Inicio</a></li>
-        <li class="nav-item"><a href="http://localhost/servicios" class="nav-link" id="menu-servicios">Servicios</a></li>
-        <li class="nav-item"><a href="#" class="nav-link" id="menu-acerca-de">Acerca de</a></li>
-        <li class="nav-item"><a href="http://localhost/contacto" class="nav-link" id="menu-contacto">Contacto</a></li>
-    </ul>
-</header>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/smartwizard@6/dist/css/smart_wizard_all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+</head>
+    <body style="margin: 0 2% 0 2%;">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <img src="<?php echo constant('URL') ?>public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
+        </a>
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="http://localhost/" class="nav-link" id="menu-inicio">Inicio</a></li>
+            <li class="nav-item"><a href="http://localhost/servicios" class="nav-link" id="menu-servicios">Servicios</a></li>
+            <li class="nav-item"><a href="#" class="nav-link" id="menu-acerca-de">Acerca de</a></li>
+            <li class="nav-item"><a href="http://localhost/contacto" class="nav-link" id="menu-contacto">Contacto</a></li>
+        </ul>
+    </header>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>  
+</html>
