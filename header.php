713f237871fe56f687d36dbb066dd1d2ce957e9e
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-01 00:51:43 -0500

    agregando al header 2

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
     <body style="margin: 0 2% 0 2%;">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <img src="<?php echo constant('URL') ?>public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
+            <img src="public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
         </a>
         <ul class="nav nav-pills">
             <li class="nav-item"><a href="http://localhost/" class="nav-link" id="menu-inicio">Inicio</a></li>
