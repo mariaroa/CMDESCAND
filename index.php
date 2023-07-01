320601f23c8ededdf4bf297851bded4ed6c1a448
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-06-30 22:44:38 -0500

    estructura basica tema

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/styles.css">
-  </head>
-  <body style="margin: 0 2% 0 2%;">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <img src="./images/cmdescand_logo.png" class="img-fluid"  alt="logo">
-        </a>   
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Inicio</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Servicios</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Acerca de</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Contacto</a></li>
-          </ul>
-    </header>
-  </body>
-</html>
+<?php get_header(); ?>
+
+<main id="main-content" class="site-main">
+    <?php
+    if (is_front_page()) {
+        // Página de inicio
+        get_template_part('template-parts/content', 'home');
+    } elseif (is_page('contacto')) {
+        // Página de contacto
+        get_template_part('template-parts/content', 'contacto');
+    } elseif (is_page('servicios')) {
+        // Página de servicios
+        get_template_part('template-parts/content', 'servicios');
+    } elseif (is_page('nosotros')) {
+        // Página de nosotros
+        get_template_part('template-parts/content', 'nosotros');
+    } elseif (is_404()) {
+        // Página de error 404
+        get_template_part('template-parts/content', '404');
+    } else {
+        // Cualquier otra página
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+                ?>
+                <article <?php post_class(); ?>>
+                    <h2><?php the_title(); ?></h2>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+                <?php
+            }
+        }
+    }
+    ?>
+</main>
+
+<?php get_footer(); ?>
