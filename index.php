30923b3be9d935f64a6ae5601a28b9b1a1df941e
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-02 18:38:03 -0500

    agregando cosas

--- index.php
+++ index.php
@@ -1,41 +1,30 @@
 <?php wp_head(); ?>
 <?php get_header(); ?>
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
+<?php endif; ?>
 
-<main id="main-content" class="site-main">
-    <?php
-    echo is_page();
-    if (is_front_page()) {
-        // Página de inicio
-        get_template_part('template-parts/content', 'home');
-    } elseif (is_page('contacto')) {
-        // Página de contacto
-        get_template_part('template-parts/content', 'contacto');
-    } elseif (is_page('servicios')) {
-        // Página de servicios
-        get_template_part('template-parts/content', 'servicios');
-    } elseif (is_page('nosotros')) {
-        // Página de nosotros
-        get_template_part('template-parts/content', 'nosotros');
-    } elseif (is_404()) {
-        // Página de error 404
-        get_template_part('template-parts/content', '404');
-    } else {
-        // Cualquier otra página
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post();
-                ?>
-                <article <?php post_class(); ?>>
-                    <h2><?php the_title(); ?></h2>
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                    </div>
-                </article>
-                <?php
-            }
-        }
-    }
-    ?>
-</main>
+<?php
+if ( have_posts() ) {
+
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+	}
+
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
+
 
 <?php get_footer(); ?>
