a9efb642a924cf5608a8e73490b04ee62e4c9a2c
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-01 21:41:14 -0500

    agregando imagenes y rutas

--- header.php
+++ header.php
@@ -12,14 +12,14 @@
 </head>
     <body style="margin: 0 2% 0 2%;">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <a href="<?php echo get_template_directory_uri(); ?>/images/cmdescand_logo.png" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img src="public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
         </a>
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="http://localhost/" class="nav-link" id="menu-inicio">Inicio</a></li>
-            <li class="nav-item"><a href="http://localhost/servicios" class="nav-link" id="menu-servicios">Servicios</a></li>
+            <li class="nav-item"><a href="<?php echo home_url('/'); ?>" class="nav-link" id="menu-inicio">Inicio</a></li>
+            <li class="nav-item"><a href="<?php echo home_url('/servicios'); ?>" class="nav-link" id="menu-servicios">Servicios</a></li>
             <li class="nav-item"><a href="#" class="nav-link" id="menu-acerca-de">Acerca de</a></li>
-            <li class="nav-item"><a href="http://localhost/contacto" class="nav-link" id="menu-contacto">Contacto</a></li>
+            <li class="nav-item"><a href="<?php echo home_url('/contacto');?>" class="nav-link" id="menu-contacto">Contacto</a></li>
         </ul>
     </header>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
