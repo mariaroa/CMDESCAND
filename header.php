6b91aea739e2e04f0d99f1f72395cb50bec4bc91
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-01 21:53:25 -0500

    parece q ya

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
 </head>
     <body style="margin: 0 2% 0 2%;">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-        <a href="<?php echo get_template_directory_uri(); ?>/public/images/cmdescand_logo.png" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <img src="public/images/cmdescand_logo.png" class="img-fluid"  alt="logo">
+        <a href="<?php echo home_url('/'); ?>" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/cmdescand_logo.png class="img-fluid"  alt="logo">
         </a>
         <ul class="nav nav-pills">
             <li class="nav-item"><a href="<?php echo home_url('/'); ?>" class="nav-link" id="menu-inicio">Inicio</a></li>
