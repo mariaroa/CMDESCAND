90c356dff94b49a03d0989012d4fc2aa61e9295f
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-01 22:58:35 -0500

    prueba url 999999

--- index.php
+++ index.php
@@ -3,16 +3,23 @@
 
 <main id="main-content" class="site-main">
     <?php
-    $queried_object = get_queried_object();
-    $page_template = get_page_template_slug($queried_object->ID);
-    echo $queried_object;
-    echo $page_template;
-
-    if ($page_template === 'content-servicios.php') {
+    if (is_front_page()) {
+        // Página de inicio
+        get_template_part('template-parts/content', 'home');
+    } elseif (is_page('contacto')) {
+        // Página de contacto
+        get_template_part('template-parts/content', 'contacto');
+    } elseif (is_page('servicios')) {
         // Página de servicios
         get_template_part('template-parts/content', 'servicios');
+    } elseif (is_page('nosotros')) {
+        // Página de nosotros
+        get_template_part('template-parts/content', 'nosotros');
+    } elseif (is_404()) {
+        // Página de error 404
+        get_template_part('template-parts/content', '404');
     } else {
-        // Resto de las páginas
+        // Cualquier otra página
         if (have_posts()) {
             while (have_posts()) {
                 the_post();
