24e9639f72b6f7cc017562a6a2e8d30dbd0bf2cb
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-02 19:43:12 -0500

    haciendo cambios 22259258

--- template-parts/content-servicios.php
+++ template-parts/content-servicios.php
@@ -1,1 +1,20 @@
-<h1>Esto es servicios</h1>
+<?php
+/*
+ * Template Name: Servicios
+ * Template Post Type: page
+ */
+
+get_header(); // Incluir el encabezado de la página
+?>
+
+<main id="main-content" class="site-main">
+    <?php
+    // Contenido específico de la página de servicios
+    while (have_posts()) {
+        the_post();
+        the_content();
+    }
+    ?>
+</main>
+
+<?php get_footer(); // Incluir el pie de página ?>
