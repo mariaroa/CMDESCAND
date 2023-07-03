5d9872e46c2ce48a2703e60a72a307be18cf8254
Jorge Leonardo Cardenas Montenegro <jorge.cardenas9006@gmail.com>
2023-07-02 21:08:24 -0500

    haciendo cambios 222592588wa

--- functions.php
+++ functions.php
@@ -3,3 +3,16 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style('my-theme-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function custom_rewrite_rule() {
+    add_rewrite_rule('^mis-paginas/([^/]*)/?', 'index.php?page_id=$matches[1]', 'top');
+}
+add_action('init', 'custom_rewrite_rule', 10, 0);
+
+function change_page_links($permalink, $post) {
+    if ($post->post_type == 'page') {
+        $permalink = home_url('/mis-paginas/' . $post->post_name . '/');
+    }
+    return $permalink;
+}
+add_filter('page_link', 'change_page_links', 10, 2);
